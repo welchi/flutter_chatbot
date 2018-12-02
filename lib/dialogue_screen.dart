084770ba1a29a914ea03ec5fffb49f9176137b9f
welchi <welchizm@gmail.com>
2018-12-03 01:01:48 +0900

    Update dialogue_screen.dart

--- lib/dialogue_screen.dart
+++ lib/dialogue_screen.dart
@@ -95,3 +95,32 @@ class _DialogueScreen extends State<DialogueScreen> {
         ));
   }
 }
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text, this.name, this.type});
+
+  final String text;
+  final String name;
+  final bool type;
+
+  List<Widget> otherMessage(context) {
+    return <Widget>[
+      new Container(
+        margin: const EdgeInsets.only(right: 16.0),
+        child: new CircleAvatar(child: new Image.asset("img/placeholder.png")),
+      ),
+      new Expanded(
+          child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Text(this.name,
+              style: new TextStyle(fontWeight: FontWeight.bold)),
+          new Container(
+            margin: const EdgeInsets.only(top: 5.0),
+            child: new Text(text),
+          ),
+        ],
+      ))
+    ];
+  }
+}
