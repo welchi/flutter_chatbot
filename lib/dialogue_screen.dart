b39a9e2a7f03b9e3da0b3ed97128efba158c63b6
welchi <welchizm@gmail.com>
2018-12-03 00:45:09 +0900

    Update dialogue_screen.dart

--- lib/dialogue_screen.dart
+++ lib/dialogue_screen.dart
@@ -68,4 +68,30 @@ class _DialogueScreen extends State<DialogueScreen> {
     });
     Response(text);
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Flutter Chatbot"),
+        ),
+        body: new Column(
+          children: <Widget>[
+            new Flexible(
+                child: new ListView.builder(
+              padding: new EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            )),
+            new Divider(
+              height: 1.0,
+            ),
+            new Container(
+              decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+              child: _buildTextComposer(),
+            ),
+          ],
+        ));
+  }
 }
