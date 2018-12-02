59c6bf33e649bffcd2c120113ebcb82addd8f166
welchi <welchizm@gmail.com>
2018-12-02 23:59:19 +0900

    Update dialogue_screen.dart

--- lib/dialogue_screen.dart
+++ lib/dialogue_screen.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dialogflow/dialogflow_v2.dart';
 
+class DialogueScreen extends StatefulWidget {
+  DialogueScreen({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _DialogueScreen createState() => new _DialogueScreen();
+}
+
+class _DialogueScreen extends State<DialogueScreen> {}
