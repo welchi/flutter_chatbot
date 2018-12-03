659c28e41031e035fbdb4e55a4a845f6e0adb319
welchi <welchizm@gmail.com>
2018-12-03 21:20:03 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Chatbot',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.cyan,
       ),
       home: DialogueScreen(),
     );
